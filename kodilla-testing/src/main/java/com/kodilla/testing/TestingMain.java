14d8872d85e360121304ba8d998de2a5bf6bb661
michal-tokarski <michal_tokarski@wp.pl>
2019-06-30 18:35:41 +0200

    Third Commit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,60 @@
 package com.kodilla.testing;
 
+/*
+ Previous version
 public class TestingMain {
     public static void main(String[] args){
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        System.out.println("Moduł 6. Wprowadzenie do testowania oprogramowania");
     }
 }
+*/
+
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.calculator.Calculator;
+
+public class TestingMain {
+
+    public static void main(String[] args){
+
+        System.out.println ("--------------------");
+        System.out.println ("1. Unit test");
+        System.out.println ("Class Simple user");
+
+        SimpleUser simpleUser = new SimpleUser ("theForumUser");
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println ("--------------------");
+        System.out.println ("2. Unit test");
+        System.out.println ("Class Calculator");
+
+        Calculator calculator = new Calculator();
+
+        int A = 20;
+        int B = 33;
+
+        System.out.println("Addition: " + A + " + " + B);
+        int result1 = calculator.add(A, B);
+        if (result1 == (A+B)){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Subtraction: " + A + " - " + B);
+        int result2 = calculator.subtract(A, B);
+        if (result2 == (A-B)){
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+    }
+}
+
+
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.kodilla.testing.calculator;
+
+public class Calculator {
+
+    public int add(int A, int B) {
+        return A + B;
+    }
+
+    public int subtract(int A, int B) {
+        return A - B;
+    }
+
+}
