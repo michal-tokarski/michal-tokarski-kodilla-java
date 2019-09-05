ae0b7cf8e029433dae3f5b08a8d5e58dd61ab685
michal-tokarski <michal_tokarski@wp.pl>
2019-09-06 00:18:36 +0200

    Task 7.5 (IntStream - Numbers Average)

--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -8,7 +8,7 @@ public class ArrayOperationsTestSuite implements ArrayOperations {
     @Test
     public void testGetAverage() {
 
-        //Given
+        //Given (ver 2)
         int numbers [] = {11,22,33,44,55,66,77,88,99,100,111,222,333,444,555,666,777,888,999,2000};
 
         //When
