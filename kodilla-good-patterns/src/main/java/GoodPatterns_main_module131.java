d2782acec7e142eaeedd1a35b7a17cfb63a411cf
michal-tokarski <michal_tokarski@wp.pl>
2019-11-17 07:57:20 +0100

    Module 13.1 - Movie Store

---
+++ kodilla-good-patterns/src/main/java/GoodPatterns_main_module131.java
@@ -0,0 +1,32 @@
+import com.kodilla.good.patterns.challenges.MovieStore;
+
+import java.util.stream.Collectors;
+
+public class GoodPatterns_main_module131 {
+
+    public static void main(String[]args) {
+
+        MovieStore myMovieStore = new MovieStore();
+
+        System.out.println("\nOriginal List :");
+        System.out.println("-----------------");
+        myMovieStore.getMovies().entrySet().stream()
+                .forEach(System.out::println);
+
+        System.out.println("\nRequested List (all titles in one row, separated by exclamation mark) :");
+        System.out.println("-------------------------------------------------------------------------");
+
+        System.out.print("Way 1 : ");
+        myMovieStore.getMovies().entrySet().stream()
+                .map(entry -> entry.getValue() + " ! ")
+                .forEach(System.out::print);
+
+        System.out.print("\nWay 2 : ");
+        String myList = myMovieStore.getMovies().entrySet().stream()
+                .flatMap(title -> title.getValue().stream())
+                .collect(Collectors.joining(" ! "));
+        System.out.println(myList);
+
+    }
+
+}
