fdeca8977748a937b9a049a90817659fc28e44ed
michal-tokarski <michal_tokarski@wp.pl>
2020-04-30 20:48:06 +0200

    Module 14.1 - Bean ForumUser

---
+++ kodilla-spring/src/main/java/com/kodilla/spring/forum/ForumUser.java
@@ -0,0 +1,17 @@
+package com.kodilla.spring.forum;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ForumUser {
+
+    private final String userName;
+
+    public ForumUser() {
+        this.userName = "John Smith";
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+}
