8d07ca16b4c741dba984f91158a32de1f4545ded
michal-tokarski <michal_tokarski@wp.pl>
2020-05-31 12:37:36 +0200

    Module 17.2 - Task Tasklists

---
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -0,0 +1,73 @@
+package com.kodilla.hibernate.tasklist;
+
+import com.kodilla.hibernate.task.Task;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "TASKLISTS")
+public final class TaskList {
+
+    private int id;
+    private String listName;
+    private String description;
+    private List<Task> pureTaskList = new ArrayList<>(); // additional field
+
+    public TaskList() {
+    }
+
+    public TaskList(String listName, String description) {
+        this.listName = listName;
+        this.description = description;
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    @Column(name = "LISTNAME")
+    public String getListName() {
+        return listName;
+    }
+
+    @Column(name = "DESCRIPTION")
+    public String getDescription() {
+        return description;
+    }
+
+    /*
+    // temporarily commented out
+    // error message : " 'Basic' attribute should not be a container "
+    public List<Task> getPureTaskList() {
+        return pureTaskList;
+    }
+    */
+
+    private void setId(int id) {
+        this.id = id;
+    }
+
+    private void setListName(String listName) {
+        this.listName = listName;
+    }
+
+    private void setDescription(String description) {
+        this.description = description;
+    }
+
+    /*
+    // temporarily commented out
+    // corresponding to the getPureTaskList() above
+    public void setPureTaskList(List<Task> pureTaskList) {
+        this.pureTaskList = pureTaskList;
+    }
+    */
+
+}
---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -0,0 +1,42 @@
+package com.kodilla.hibernate.tasklist.dao;
+
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TaskListDaoTestSuite {
+
+    @Autowired
+    private TaskListDao taskListDao;
+
+    private static final String TASKLIST_NAME = "My tasklist";
+    private static final String TASKLIST_DESCRIPTION = "My description";
+
+    @Test
+    public void testFindByListName() {
+
+        // Given
+        TaskList taskList = new TaskList(TASKLIST_NAME, TASKLIST_DESCRIPTION);
+        taskListDao.save(taskList);
+        String listName = taskList.getListName();
+
+        // When
+        List<TaskList> readTaskLists = taskListDao.findByListName(listName);
+
+        // Then
+        Assert.assertEquals(1, readTaskLists.size());
+
+        // CleanUp
+        int id = readTaskLists.get(0).getId();
+        taskListDao.deleteById(id);
+    }
+
+}
